6a87a2eb902756e9cf8d7ee01215f84e4b2c1d67
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-26 16:10:07 -0300

    mudando 0 para 1 em cont

--- src/ArrayJava/Array.java
+++ src/ArrayJava/Array.java
@@ -2,10 +2,10 @@ package ArrayJava;
 
 public class Array {
 	public static void main(String[] args) {
-		int cont = 0;
+		int cont = 1;
 		double a[] = {1,8,2.5,1.4,1.1};
 		for (double i:a) {
-			System.out.print(i + ( cont < a.length-1 ? " -> " : " "));
+			System.out.print(i + ( cont < a.length ? " -> " : " "));
 			cont++;
 		}
 	}
